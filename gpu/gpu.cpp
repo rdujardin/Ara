9c608c72f43d57ee0981210e0cd118d6a1ebdcfe
Raphaël Dujardin <rdujardin@rdujardin.com>
2016-05-18 00:16:36 +0200

    Signed-off-by: Raphaël Dujardin <rdujardin@rdujardin.com>

--- gpu/gpu.cpp
+++ gpu/gpu.cpp
@@ -1,3 +1,32 @@
+/*
+gpu.cpp (part of Ara, https://github.com/rdujardin/Ara)
+
+Copyright (c) 2016, Raphaël Dujardin (rdujardin) & Jean Boehm (jboehm1)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
 //g++ -o gpu gpu.cpp gpuGlContext.cpp gpuShader.cpp gpuTexture.cpp -lGLESv2 -lEGL -I/opt/vc/include -L/opt/vc/lib -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -lbcm_host -lpthread `pkg-config --cflags --libs opencv`
 
 #include <iostream>
